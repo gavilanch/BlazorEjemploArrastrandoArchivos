544cf14a2a651293e0112bc2d0377e2c05162414
Felipe Gavilan <elmejor_887@hotmail.com>
2023-02-21 16:20:31 -0400

    Add project files.

---
+++ EjemploArrastrandoArchivos/wwwroot/js/pegarArchivo.js
@@ -0,0 +1,17 @@
+﻿export function inicializarPegadoDeArchivo(contenedorDestinoArrastre, inputFile) {
+
+    contenedorDestinoArrastre.addEventListener('paste', onPaste);
+
+    function onPaste(event) {
+        inputFile.files = event.clipboardData.files;
+        const changeEvent = new Event('change', { bubbles: true });
+        inputFile.dispatchEvent(changeEvent);
+    }
+
+    return {
+        dispose: () => {
+            contenedorDestinoArrastre.removeEventListener('paste', onPaste);
+        }
+    }
+
+}
